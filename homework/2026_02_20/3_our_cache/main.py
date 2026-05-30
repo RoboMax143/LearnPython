e62fa16175fd0ce777a33ed9168bd63396adcc30
RoboMax143 <navas23154@gmail.com>
2026-05-31 00:01:06 +0200

    Update main.py

--- homework/2026_02_20/3_our_cache/main.py
+++ homework/2026_02_20/3_our_cache/main.py
@@ -1,5 +1,23 @@
 def cache(func):
-    pass
+    cached_results = dict()
 
+    def wrapper(*args):
+        key = args
+        if key not in cached_results:
+            cached_results[key] = func(*args)
+        return cached_results[key]
+    
+    return wrapper
+
+
+@cache
 def my_sum(a, b):
+    print(f"Вычисляю сумму {a} + {b}")
     return a + b
+
+
+if __name__ == "__main__":
+    print(my_sum(2, 3))
+    print(my_sum(2, 3))
+    print(my_sum(4, 5))
+    print(my_sum(4, 5))
