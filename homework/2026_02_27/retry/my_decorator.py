98e8b7f411fc5f5398cdd9fba21cd7914fae75dc
RoboMax143 <navas23154@gmail.com>
2026-06-01 09:15:48 +0200

    Update my_decorator.py

--- homework/2026_02_27/retry/my_decorator.py
+++ homework/2026_02_27/retry/my_decorator.py
@@ -1,2 +1,25 @@
-def retry(count):
-    pass
+import functools
+
+def retry(count=5):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            last_exception = None
+            for _ in range(count):
+                try:
+                    return func(*args, **kwargs)
+                except ValueError:
+                    last_exception = ValueError
+                    continue
+                except OSError:
+                    print(f"{func.__name__} raise OsError exception.")
+                    last_exception = OSError
+                    continue
+                except Exception as e:
+                    raise e
+                    
+            if last_exception:
+                raise last_exception
+                
+        return wrapper
+    return decorator
